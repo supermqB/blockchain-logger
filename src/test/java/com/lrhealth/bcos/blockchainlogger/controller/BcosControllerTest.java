66c10c95de39b6bb27b8eee21572b841bda27c87
Wen Bao Yin <supermq@gmail.com>
2021-05-18 13:27:41 +0800

    complete ci/cd

--- src/test/java/com/lrhealth/bcos/blockchainlogger/controller/BcosControllerTest.java
+++ src/test/java/com/lrhealth/bcos/blockchainlogger/controller/BcosControllerTest.java
@@ -7,7 +7,10 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
+import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -17,33 +20,32 @@ import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 @SpringBootTest
 @AutoConfigureMockMvc
+@TestMethodOrder(OrderAnnotation.class)
 public class BcosControllerTest {
     @Autowired
     private MockMvc mvc;
 
-    @Test
-    public void getHello() throws Exception {
-        mvc.perform(MockMvcRequestBuilders.get("/").accept(MediaType.APPLICATION_JSON)).andExpect(status().isOk())
-                .andExpect(content().string(equalTo("yes, I'm done!")));
-    }
+    private static String savedLogid;
 
     @Test
+    @Order(1) 
     public void testAddlog() throws JsonProcessingException, Exception {
         ObjectMapper objectMapper = new ObjectMapper();
-        String logid = mvc
+        savedLogid = mvc
                 .perform(MockMvcRequestBuilders.post("/addlog").contentType(MediaType.APPLICATION_JSON)
                         .content(objectMapper.writeValueAsString(
                                 new LogRequestBean("yinwenbao", "/api/test/add", "test content..."))))
                 .andExpect(status().isOk()).andReturn().getResponse().getContentAsString();
-        System.out.println(logid);
+        System.out.println(savedLogid);
     }
 
     @Test
+    @Order(2) 
     public void testVerifylog() throws JsonProcessingException, Exception {
         ObjectMapper objectMapper = new ObjectMapper();
         mvc.perform(MockMvcRequestBuilders.post("/verifylog").contentType(MediaType.APPLICATION_JSON)
                 .content(objectMapper.writeValueAsString(
-                        new VerifyRequestBean("979e2087df4b80a5dca377766cc8465f", "test content..."))))
+                        new VerifyRequestBean(savedLogid, "test content..."))))
                 .andExpect(status().isOk()).andExpect(content().string(equalTo("true")));
     }
 }
